200f756d7bbb9cdb2e81e17e5d629fed36dc11c0
Dan Stevens <dan.stevens@oracle.com>
2015-04-01 15:58:22 +0100

    Turns out there is IntelliSense for Data Binding in Visual Studio 2013 (http://blogs.msdn.com/b/visualstudio/archive/2013/08/09/xaml-editor-improvements-in-visual-studio-2013.aspx), except I can't get it working for some reason!

---
+++ IntelliSenseForDataBinding/IntelliSenseForDataBinding/MainViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace IntelliSenseForDataBinding
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public MainViewModel()
+        {
+            Greeting = "Hello World";
+            Answer = 42;
+        }
+
+        private string _Greeting;
+        public string Greeting
+        {
+            get { return _Greeting; }
+            set { _Greeting = value; OnPropertyChanged(); }
+        }
+
+        private int _Answer;
+        public int Answer
+        {
+            get { return _Answer; }
+            set { _Answer = value; OnPropertyChanged(); }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+                handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
